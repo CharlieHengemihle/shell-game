1d6049cbc95284a6b078735474cdbc4589f49fe9
Charlie Hengemihle <charliehengemihle@gmail.com>
2022-09-12 18:23:29 -0600

    added a hidePearl function as well as listeners

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import { getRandomItem } from './utils.js';
 // import { getRandomItem } from './utils.js';
 
 /* State */
-let gameState = 'guess'; //'guess' or 'results'
+let gameState = 'results'; //'guess' or 'results'
 let guess = 'shell-1'; // 'shell-1' 'shell-2' or 'shell-3'
 let reveal = 'pearl-1'; //'pearl-1' 'pearl-2' or 'pearl-3'
 let results = 'Found it'; //'Found it!' or 'Not Here!'
@@ -11,31 +11,73 @@ let results = 'Found it'; //'Found it!' or 'Not Here!'
 /* Actions */
 function loadPage() {
     displayShells();
-    displayResults();
+    // displayResults();
     displayScoreboard();
 }
 
 const guessOne = document.getElementById('guess-1');
 const guessTwo = document.getElementById('guess-2');
 const guessThree = document.getElementById('guess-3');
+
+const shell1 = document.getElementById('shell-1');
+const shell2 = document.getElementById('shell-2');
+const shell3 = document.getElementById('shell-3');
+
+const pearl1 = document.getElementById('pearl-1');
+const pearl2 = document.getElementById('pearl-2');
+const pearl3 = document.getElementById('pearl-3');
+
+const display1 = document.getElementById('display-1');
+const display2 = document.getElementById('display-2');
+const display3 = document.getElementById('display-3');
+
+const playAgainButton = document.getElementById('play-again-button');
 // event listeners
 function displayShells() {
-    guessOne.classList.remove('display-1', 'display-2', 'display-3');
-    guessTwo.classList.remove('display-1', 'display-2', 'display-3');
-    guessThree.classList.remove('display-1', 'display-2', 'display-3');
-
+    if (gameState === 'guess') {
+        pearl1.classList.add('hidden');
+        pearl2.classList.add('hidden');
+        pearl3.classList.add('hidden');
+        shell1.classList.remove('reveal');
+        shell2.classList.remove('reveal');
+        shell3.classList.remove('reveal');
+        display1.classList.add('hidden');
+        display2.classList.add('hidden');
+        display3.classList.add('hidden');
+        playAgainButton.classList.add('hidden');
+    }
     if (gameState === 'results') {
+        guessOne.classList.add('hidden');
+        guessTwo.classList.add('hidden');
+        guessThree.classList.add('hidden');
         if (guess === 'guess-1') {
-            guessOne.classList.add('display-1');
+            shell1.classList.add('reveal');
         } else {
-            guessOne.classList.edd('display-3');
+            guessOne.classList.add('display-3');
         }
     }
 }
 
+guessOne.addEventListener('click', () => {
+    hidePearl('guessOne');
+});
+guessTwo.addEventListener('click', () => {
+    hidePearl('guessTwo');
+});
+guessThree.addEventListener('click', () => {
+    hidePearl('guessThree');
+});
 /* Components */
-
-function playAgain() {}
+function hidePearl(userGuess) {
+    gameState = 'results';
+    guess = userGuess;
+    reveal = getRandomItem(array);
+    total++;
+}
+function playAgain() {
+    gameState = 'guess';
+    loadPage();
+}
 
 /* Component */
 // get DOM
